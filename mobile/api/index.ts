30d379ec497ba4d8d7d31a16ec9fcbbf4d137556
Compulink-Dev <devcompulink@gmail.com>
2025-09-30 12:27:46 +0200

    render

--- mobile/api/index.ts
+++ mobile/api/index.ts
@@ -1,4 +1,4 @@
-// export const API_URL = "https://budget-wise-ihvz.onrender.com/api";
+export const API_URL = "https://budget-wise-ihvz.onrender.com/api";
 // export const API_URL = "http://localhost:5000/api"; 
 // export const API_URL = http://192.168.0.101:5000/api
-export const API_URL = "http://10.0.2.2:5000/api";
+// export const API_URL = "http://10.0.2.2:5000/api";
